38bd0562963547bba7c6dc78c5fd20b94c0d6270
shan201637 <18312015269@163.com>
2019-11-28 21:12:32 +0800

    加载静态资源
    加载静态资源

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ app.on('request', (req, res) => {
         fs.readFile(path.join(__dirname, './hero.json'), 'utf8', (err, data) => {
             if (err) return console.log(err.message);
             let herosArr = JSON.parse(data)
-            let str = template(path.join(__dirname, './views/index.html'), {data:herosArr})
+            let str = template(path.join(__dirname, './views/index.html'), {
+                data: herosArr
+            })
             res.end(str);
         })
 
@@ -40,6 +42,15 @@ app.on('request', (req, res) => {
         res.end(str);
     } else if (method == 'GET' && pathname == '/node_modules/bootstrap/dist/css/bootstrap.css') {
         fs.readFile(path.join(__dirname, '/node_modules/bootstrap/dist/css/bootstrap.css'), (err, data) => {
+            if (err) return console.log(err.message);
+            // 为了浏览器端没有警告，需要添加一个响应头
+            res.writeHeader(200, {
+                'Content-Type': 'text/css;charset=utf-8'
+            })
+            res.end(data)
+        })
+    } else if (method == 'GET' && pathname == '/js/jquery-1.12.2.js') {
+        fs.readFile(path.join(__dirname, './js/jquery-1.12.2.js'), (err, data) => {
             if (err) return console.log(err.message);
             res.end(data)
         })
